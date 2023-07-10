d8f0768b5eea0462ee938752ad1ebfcd63baab49
David Bronleewe <david@bronleewe.net>
2023-07-10 15:49:46 -0700

    added invaders switch

--- src/main.rs
+++ src/main.rs
@@ -15,30 +15,38 @@ fn choose_structure_bonus(mut rng: &mut impl Rng) {
 	println!("{}", structure_bonuses[0]);
 }
 
-fn init_factions(mut rng: &mut impl Rng) -> Vec<&'static str>{
+fn init_factions(mut rng: &mut impl Rng, invaders_from_afar: bool) -> Vec<&'static str>{
 	let mut factions = vec![
 		"Nordic",
 		"Rusviet",
-		"Togawa",
 		"Crimea",
 		"Saxony",
 		"Polania",
-		"Albion",
 	];
+
+	if invaders_from_afar {
+		factions.push("Albion");
+		factions.push("Togawa");
+	}
+
 	factions.shuffle(&mut rng);
 	factions
 }
 
-fn init_player_mats(mut rng: &mut impl Rng) -> Vec<&'static str> {
+fn init_player_mats(mut rng: &mut impl Rng, invaders_from_afar: bool) -> Vec<&'static str> {
 	let mut player_mats = vec![
 		"Industrial",
 		"Engineering",
-		"Militant",
 		"Patriotic",
-		"Innovative",
 		"Mechanical",
 		"Agricultural",
 	];
+
+	if invaders_from_afar {
+		player_mats.push("Militant");
+		player_mats.push("Innovative");
+	}
+
 	player_mats.shuffle(&mut rng);
 	player_mats
 }
@@ -47,20 +55,23 @@ fn init_player_mats(mut rng: &mut impl Rng) -> Vec<&'static str> {
 struct Args {
 	#[arg(default_value_t = 5)]
 	player_count: u8,
+	#[arg(short, long)]
+	invaders_from_afar: bool,
 }
 
 fn main() {
 	let args = Args::parse();
-	if args.player_count < 1 || args.player_count > 7 {
-		println!("Player count must be from 1 to 7");
+	let max_player_count = if args.invaders_from_afar { 7 } else { 5 };
+	if args.player_count < 1 || args.player_count > max_player_count {
+		println!("Player count must be from 1 to {}", max_player_count);
 		return;
 	}
 
 	let mut rng = rand::thread_rng();
 	println!("Scythe Setup:");
 	choose_structure_bonus(&mut rng);
-	let factions = init_factions(&mut rng);
-	let player_mats = init_player_mats(&mut rng);
+	let factions = init_factions(&mut rng, args.invaders_from_afar);
+	let player_mats = init_player_mats(&mut rng, args.invaders_from_afar);
 
 	for i in 0..args.player_count {
 		println!("Player {}: {} {}",
